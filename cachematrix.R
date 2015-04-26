fb852d19fab868f407d8e2999034c434ffe47a52
durgamahesh16 <durga16688@gmail.com>
2015-04-26 17:16:51 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -31,3 +31,57 @@ cacheSolve <- function(x) {
         endingmatrix                            
 }
 
+#
+# Test Results:
+# 
+# > m <- makeCacheMatrix()
+# > m$set(matrix(c(0,2,2,0),2,2))
+# > m$get()
+# [,1] [,2]
+# [1,]    0    2
+# [2,]    2    0
+# > cacheSolve(m)
+#      [,1] [,2]
+# [1,]  0.0  0.5
+# [2,]  0.5  0.0
+# > cacheSolve(m)
+# getting cached data
+#      [,1] [,2]
+# [1,]  0.0  0.5
+# [2,]  0.5  0.0
+# > 
+# 
+# Change to matrix triggers fresh inversion solution:
+#
+# > m$set(matrix(c(100,2,2,100),2,2))
+# > m$get()
+# [,1] [,2]
+# [1,]  100    2
+# [2,]    2  100
+# > cacheSolve(m)
+# [,1]        [,2]
+# [1,]  0.01000400 -0.00020008
+# [2,] -0.00020008  0.01000400
+# > cacheSolve(m)
+# getting cached data
+# [,1]        [,2]
+# [1,]  0.01000400 -0.00020008
+# [2,] -0.00020008  0.01000400
+# 
+# Change to matrix triggers fresh inversion solution:
+#
+# > m$set(matrix(c(0,2,2,0),2,2))
+# > m$get()
+# [,1] [,2]
+# [1,]    0    2
+# [2,]    2    0
+# > cacheSolve(m)
+# [,1] [,2]
+# [1,]  0.0  0.5
+# [2,]  0.5  0.0
+# > cacheSolve(m)
+# getting cached data
+# [,1] [,2]
+# [1,]  0.0  0.5
+# [2,]  0.5  0.0
+# > 
